6d2f29d8d8a01279de70dcb321d42686ae5cbd12
Barlow Tucker <barlowtucker@gmail.com>
2017-02-10 13:52:49 -0700

    Initial Commit

---
+++ Challenges/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Challenges
+//
+//  Created by Barlow Tucker on 2/10/17.
+//  Copyright © 2017 Barlow Tucker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
